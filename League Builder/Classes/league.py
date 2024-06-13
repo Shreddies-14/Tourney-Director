7be8e5e99a4ca96e897ee1a2f96d68612894433d
Cameron Whyte <cameronwhyte399@gmail.com>
2024-06-13 17:29:44 +0100

    New League Builder Started

--- League Builder/Classes/league.py
+++ League Builder/Classes/league.py
@@ -5,14 +5,82 @@
 
 import itertools
 import logging
+import pandas
+import main
+
+pandas.__version__
 
 logger = logging.getLogger(__name__)
 
 class leagueClass:
 
     id_iter = itertools.count() #Creates a counter 
+    leagueOutput = []
 
     def __init__(self):
         self.id = next(self.id_iter) #Creates unique ID
         logger.info('Created Class with unique ID {}'.format(self.id))
+
+    def new_league(self):
+        #This will create a brand new league from scratch asking questions about it
+        print("Welcome to the new league builder!\n")
+        logger.info("New league builder started!")
+
+        #Get values
+        playerCount = main.get_Valid_Integer("How many players do you have?\n")
+        divisionCount = main.get_Valid_Integer("How many divisions do you want?\n")
+        logger.info("User entered {} for playerCount and {} for divisionCount!".format(playerCount, divisionCount))
+
+
+        #Init values
+        divisionPlayerSplit = [divisionCount]
+        playersLeft = playerCount
+        validTotalDivisionPlayerSplit = False
+        validDivisionPlayerSplit = True
+
+        if playerCount/divisionCount >= 2: #Check there is more than 2 per division
+            while(not validTotalDivisionPlayerSplit): #Loop until a valid splits is entered
+                x = 0
+
+                while (x < divisionCount and validDivisionPlayerSplit): #Loop until all divisions are valid 
+                    validUserInput = False
+
+                    if (playersLeft < 2): #If there is less than 2 players then a valid division cannot be created
+                        validDivisionPlayerSplit = False
+                        print("Invalid split has been entered, please retry!\n")
+
+                    while (not validUserInput): #Loop until valid split is entered
+                        userDivisionSplitInput = main.get_Valid_Integer("How many people do you want in division {}? (Players left:{})\n".format(x + 1, playersLeft))
+
+                        if userDivisionSplitInput <= playersLeft: #If input is a valid number
+                            
+                            divisionPlayerSplit[x] = userDivisionSplitInput
+                            playersLeft = playersLeft - userDivisionSplitInput
+
+                            validUserInput = True
+                            x = x + 1
+
+                        else:
+                            print("Invalid Input please try again\n")
+
+                for x,i in enumerate(divisionPlayerSplit): #Loop through all splits to confirm inputs to user
+                    print("Division {} has {} players in it!\n".format(i + 1,x))
+                
+                validConfirmation = False
+
+                while (not validConfirmation): #Loop until valid confirmation
+                    userComfirmationInput = input("If this is correct type 'YES' else type 'NO'!\n")
+
+                    if userComfirmationInput == "YES": 
+                        validConfirmation = True
+                        validTotalDivisionPlayerSplit = True
+
+                    elif userComfirmationInput == "NO":
+                        validConfirmation = True
+
+                    else:
+                        print("Invalid input please try again\n")
+
+
+
 
--- League Builder/main.py
+++ League Builder/main.py
@@ -10,9 +10,55 @@ import datetime
 logger = logging.getLogger(__name__)
 
 def main():
-    logging.basicConfig(filename='League Builder/Logs/logs.log', level=logging.INFO)
-    logger.info('Program Started at {}'.format(datetime.datetime.now(datetime.timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')))
-    logger.info('Program Finished at {}'.format(datetime.datetime.now(datetime.timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')) )
+    logging.basicConfig(filename='League Builder/Logs/logs.log', level=logging.DEBUG)
+    logger.debug("------------------------------------------------------------------------------------------------")
+    logger.debug('Program Started at {}'.format(datetime.datetime.now(datetime.timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')))
+
+#Define values
+    userInput = 0
+    validUserInput = False
+
+#Welcome user
+    print("Welcome to Cameron Whyte's League Builder!")
+    print("Please enter the number of the action you want to perform.")
+    print("1.Create a league")
+    
+    logger.info("Welcome complete waiting on user input")
+
+#Get user input for what they want to do
+    while not validUserInput: #Loop until valid input entered
+        userInput = get_Valid_Integer("Enter the number!")
+
+        if userInput > 0 and userInput < 2: #Check value is an enterable integer
+            logger.info("User entered {}".format(userInput))
+            validUserInput = True
+            continue #Go back to start of loop
+
+        logger.error("User entered {}, this was not an option".format(userInput))
+
+    match int(userInput): #Matches the input to the correct function call
+        case 1: #Create new league
+            logger.info("Starting Creating a new League")
+            newCreatedLeague = league.leagueClass() #This will create a new league table and write it to a CSV file
+            newCreatedLeague.new_league()
+    
+
+    logger.debug('Program Finished at {}'.format(datetime.datetime.now(datetime.timezone.utc).strftime('%a, %d %b %Y %H:%M:%S GMT')) )
+
+def get_Valid_Integer(outputString):
+    validUserInput = False
+    while not validUserInput: #Loop until valid input entered
+        userInput = input(outputString)
+
+        try: #Check value is a integer
+            val = int(userInput)
+
+        except ValueError: #If error
+            logger.error("User entered {}, this threw an error".format(userInput))
+            print("Invalid value entered please try again!")
+            continue #Go back to start of loop
+        
+        return int(userInput)
 
 if __name__ == '__main__':
     main()
